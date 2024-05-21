d36ada88d19751bf4fac4c75bf7907fc0b323803
Dmitriy Naumenko <naumenko.dmitriy@gmail.com>
2024-05-20 19:57:45 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dmitriy Naumenko on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
